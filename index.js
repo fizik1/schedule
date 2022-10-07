20a91577f2d504ebc9dbda928383f97551ac3a55
fizik1 <shahobnarpay@gail.com>
2022-10-07 18:49:54 +0500

    s

--- index.js
+++ index.js
@@ -2,21 +2,65 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 
+
+var groups= new Set()
+groups.add('salom')
+console.log(groups);
+
 app.get("/", async (req, res) => {
-  // res.send(new Date(1630886400*1000))
   let newItems = [];
-  for(let i=0;i<=50;i++){
-    console.log(i);
+  var pageCount=0
+
+
+  await axios.get("https://student.samdu.uz/rest/v1/data/schedule-list", {
+      headers:{
+        accept:"application/json",
+        Authorization:"Bearer dpbJRafHgNO28kk30iU_0XdAgOziHWTo"
+      }
+    }).then(javob=>{
+      pageCount=javob.data.data.pagination.pageCount
+      console.log(javob.data.data.items[0]);
+    })
+  for(let i=0;i<=10;i++){
+    await axios.get("https://student.samdu.uz/rest/v1/data/schedule-list", {
+      headers:{
+        accept:"application/json",
+        Authorization:"Bearer dpbJRafHgNO28kk30iU_0XdAgOziHWTo"
+      },
+      params:{
+        year:2022,
+        page:i
+      }
+    }).then(javob=>{
+      // console.log(javob.data.data.items);
+      console.log(i);
+      javob.data.data.items.forEach(element => {
+        newItems.push(element)
+      });
+    })
+    
   }
-  
-  // console.log(response);
-  // newItems.push(response.data.data.items)
-  // response.data.data.items.forEach(element => {
-  //   newItems.push(element)
-  // });
-  // console.log(newItems);
-  // console.log(response(3))
-  // res.send(JSON.stringify(newItems))
+  var faculties = []
+  newItems.forEach(item=>{
+    // console.log(faculties[item.faculty.id]);
+    if(faculties[0]){
+      console.log("b");
+      faculties.map((item1, index)=>{
+        console.log(item1.id);
+        // if(item.faculty.id!=item1.id){
+        //   console.log("c");
+        //   faculties.push({id:item.faculty.id, name:item.faculty.name})
+        // }
+      })
+    }else{
+      console.log("a");
+      faculties.push({id:item.faculty.id, name:item.faculty.name})
+    }
+  })
+
+  console.log(faculties);
+
+
 });
 
 app.listen(3000, () => {
